2e9459583c73ac9092d45e98fa54d52c9751ef1f
TuringDisciple <mncubenashe@gmail.com>
2018-06-19 11:52:57 +0100

    Typeclasses done

--- parser.ml
+++ parser.ml
@@ -4,6 +4,7 @@
 
 (* Useful types *)
 type 'a maybe  = Nothing | Just of 'a
+
 (* A parser type takes in a string, and produces a list of possible parses *)
 type 'a parser = Parser of (string -> (string * 'a) list)
 
@@ -11,16 +12,24 @@ type 'a parser = Parser of (string -> (string * 'a) list)
 let parse_maybe px s =
    match px s with
    | []          -> Nothing
-   | pair::pairs -> let (_, x) = pair in Just x
+   | p::ps -> let (_, x) = p in Just x
 
 (* Here's a parser that simply tries to get a character from an input string
- * val item : string parser *)
-let item = Parser(fun s ->
-    match String.explode s with
-    | []   -> []
-    | x::xs -> [ (String.implode xs, x) ] )
+ * val item : char parser *)
+let item = Parser( fun s ->
+    match s with
+    | ""   -> []
+    | _ when String.length s = 1  -> [ ("", s.[0]) ]
+    | _    ->
+      let t = (String.sub s 1 ( ( String.length s ) - 1)) in [ (t, s.[0]) ] )
 
 (* Useful functions *)
+(* val tail_of_string : string -> string *)
+let tail s =
+   match s with
+   | _ when String.length s <= 1  -> ""
+   | _ -> let t = (String.sub s 1 ( ( String.length s ) - 1)) in t
+
 (* val snd   : 'a * 'b -> 'b *)
 let snd pair = let (_, s) = pair in s
 
@@ -48,7 +57,10 @@ let (<|) f v   = f v
 (* val flip  : ('a -> 'b -> 'c ) -> 'b -> 'a -> 'c *)
 let flip f x y = f y x
 
-(* Functor *)
+let (+:) x l = x :: l
+
+(* Functor
+ * Functors give us the ability to modify the output of parses *)
 module type Functor = sig
    type 'a f
    val fmap  : ( 'a -> 'b ) -> 'a f -> 'b f
@@ -71,29 +83,31 @@ end
 
 module ParserFunctor : Functor with type 'a f = 'a parser = struct
    type 'a f = 'a parser
-   let fmap f = function
+   let fmap g = function
       Parser px -> Parser( fun s ->
          List.map
-            ( fun pair -> let (ts, x) = pair in (ts, f x) )
+            ( fun pair -> let (ts, x) = pair in (ts, g x) )
             ( px s ) )
    let (<$>) = fmap
+   (* Derived combi  *)
    let (<$) a = function
       Parser _ -> Parser(fun s -> [ (s, a) ])
 end
 
-(* Applicative *)
+(* Applicative
+ * Applicative gives us the ability to chain parsers through <*> *)
 module type Applicative = sig
-   include Functor
-   type 'a f'
-   val pure : 'a -> 'a f'
-   val (<*>): ( 'a -> 'b ) f' -> 'a f' -> 'b f'
-   val (<*<):       'a f'     -> 'b f' -> 'a f'
-   val (>*>):       'a f'     -> 'b f' -> 'b f'
+   type 'a f
+   include Functor with type 'a f := 'a f
+   val pure : 'a -> 'a f
+   val (<*>): ( 'a -> 'b ) f -> 'a f -> 'b f
+   val (<*<):       'a f     -> 'b f -> 'a f
+   val (>*>):       'a f     -> 'b f -> 'b f
+   val (<:>):       'a f     -> ( 'a list ) f -> ( 'a list ) f
 end
 
-module ParserApp : Applicative with type 'a f' = 'a parser = struct
+module ParserApp : ( Applicative with type 'a f = 'a parser ) = struct
    include ParserFunctor
-   type 'a f' = 'a parser
    let pure x      = Parser(fun s -> [ (s, x) ])
    let (<*>) ( Parser pf ) ( Parser px ) =
       Parser( fun s ->
@@ -103,8 +117,55 @@ module ParserApp : Applicative with type 'a f' = 'a parser = struct
                      ( fun pair_ -> let (sss, x ) = pair_ in (sss, ( fs x )) )
                      ( px ss ) )
             ( pf s ) )
+   (* Derived combi *)
    let (<*<) px py = const <$> px <*> py
    let (>*>) px py = id <$ px <*> py
+   let (<:>) x xs  = (+:) <$> x <*> xs
 end
 
-(* Alternative *)
+(* Alternative
+ * Alternative gives us choices between parsers via <|> *)
+module type Alternative = sig
+   type 'a f
+   (* include Functor with type 'a f := 'a f *)
+   include Applicative with type 'a f := 'a f
+   val empty : 'a f
+   val (<|>) : 'a f -> 'a f -> 'a f
+   val some  : 'a f -> ('a list) f
+   val many  : 'a f -> ('a list) f
+end
+
+module ParserAlt : ( Alternative with type 'a f := 'a parser ) = struct
+   include ParserApp
+   (* include ParserFunctor *)
+   let empty = Parser( fun _ -> [] )
+   let (<|>) ( Parser px ) ( Parser py ) =
+      Parser( fun s -> List.append ( px s ) ( py s ) )
+
+   (* Derived combi *)
+   let rec some px = px <:> some px <|> empty
+   let     many px = some px <|> empty
+end
+
+(* Monad
+ * The Monad typeclass allows for the output of one parser to afect the final
+ * output via >>= *)
+module type Monad = sig
+   type 'a f
+   include Alternative with type 'a f := 'a f
+   val return : 'a -> 'a f
+   val (>>=)  : 'a f -> ('a -> 'b f) -> 'b f
+end
+
+module ParserMonad : ( Monad with type 'a f := 'a parser)  = struct
+   include ParserAlt
+   let return x = pure x
+   let (>>=) ( Parser px ) f =
+      Parser( fun s ->
+         List.flatten <| List.map
+            ( fun p -> let (ss, x) = p in
+               match ( f x ) with Parser px -> ( px ss ))
+            ( px s ) )
+end
+
+(* Parsing functions *)
